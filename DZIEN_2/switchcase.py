6731b0f2062a809d31d37972e38de1251a2d014e
albim72 <marcin.albiniak72@gmail.com>
2022-11-15 14:27:25 +0100

    Create switchcase.py

---
+++ DZIEN_2/switchcase.py
@@ -0,0 +1,36 @@
+class Switch(object):
+    value=None
+    def __new__(cls, value):
+        cls.value=value
+        return True
+
+
+def case(*args):
+    return any((Switch.value == arg for arg in args))
+
+
+
+while Switch(int(input("podaj cyfrę (0-9): "))):
+    if case(0):
+        print("n jest równe 0")
+        break
+    if case(1,4,9):
+        print("n jest kwadratem innej cyfry!")
+        break
+    if case(2):
+        print("n jest liczbą parzystą")
+    if case(2,3,5,7):
+        print("n jest liczbą pierwszą")
+        break
+    if case(6,8):
+        print("n jest krotnością liczby 2")
+        break
+    print("pisz tylko cyfry!")
+
+m = int(input("podaj wartość dla Switch: "))
+s1 = Switch(m)
+s2 = Switch(56)
+
+print(s1==s2)
+
+
