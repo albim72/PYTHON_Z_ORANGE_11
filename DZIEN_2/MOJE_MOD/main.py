5f49599bcc5fd3c5905b314acc6e8c103a13f9fe
albim72 <marcin.albiniak72@gmail.com>
2022-11-15 09:40:50 +0100

    Update main.py

--- DZIEN_2/MOJE_MOD/main.py
+++ DZIEN_2/MOJE_MOD/main.py
@@ -1,6 +1,7 @@
 #import dane
 #import dane as dn
 from funkcje_c.bfunkcje import czytaj,czytaj_dict
+from klasy_c.cklasa import CDane
 
 from dane import book as ksiazka
 from dane import nb as liczby
@@ -13,3 +14,9 @@ print(list(enumerate(liczby,101)))
 
 czytaj(liczby)
 czytaj_dict(ksiazka)
+
+print("_________________ działanie z klasy __________________")
+
+cd = CDane(liczby,ksiazka)
+cd.czytaj_lista()
+cd.czytaj_slownik()
