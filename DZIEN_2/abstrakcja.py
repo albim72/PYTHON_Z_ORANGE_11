e3cc019c6c5faa5dcbf5b6c8b7a6590c6e83ebe9
albim72 <marcin.albiniak72@gmail.com>
2022-11-15 12:24:18 +0100

    Update abstrakcja.py

--- DZIEN_2/abstrakcja.py
+++ DZIEN_2/abstrakcja.py
@@ -1,17 +1,31 @@
 from abc import ABC, abstractmethod
 
 class Prototyp(ABC):
-    
+
     def __init__(self,x):
         self.x = x
-        
+
     def msg(self):
         return "to jest metoda nieabstrakcyjna klasy abstrakcyjnej"
-    
+
     @abstractmethod
     def policz(self):
         pass
-    
+
     @abstractmethod
     def policz_x(self):
         return self.x*3
+
+class ZwKlasa(Prototyp):
+    def __init__(self,x,y):
+        super().__init__(x)
+        self.y=y
+
+    def policz(self):
+        return 999
+
+    def policz_x(self):
+        return super().policz_x() + self.y*4
+
+
+zw = ZwKlasa(4,8)
