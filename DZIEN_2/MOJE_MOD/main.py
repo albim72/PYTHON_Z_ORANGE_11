b4fcfb1fec73800d80733ff3af5acc4afa00de2f
albim72 <marcin.albiniak72@gmail.com>
2022-11-15 09:34:33 +0100

    Update main.py

--- DZIEN_2/MOJE_MOD/main.py
+++ DZIEN_2/MOJE_MOD/main.py
@@ -1,8 +1,15 @@
 #import dane
 #import dane as dn
+from funkcje_c.bfunkcje import czytaj,czytaj_dict
 
 from dane import book as ksiazka
 from dane import nb as liczby
 
 print(ksiazka)
 print(liczby)
+
+print(list(enumerate(ksiazka)))
+print(list(enumerate(liczby,101)))
+
+czytaj(liczby)
+czytaj_dict(ksiazka)
