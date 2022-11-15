784e31f96708b4d80affb9f0f7709ed77d7edf67
albim72 <marcin.albiniak72@gmail.com>
2022-11-15 11:23:17 +0100

    Update osoba.py

--- DZIEN_2/OSOBA/osoba.py
+++ DZIEN_2/OSOBA/osoba.py
@@ -19,10 +19,10 @@ class Osoba:
 
     def czypracownik(self):
         return False
-    
+
     def bmi(self):
         return self.waga/(self.wzrost/100)**2
-    
+
     def opis_bmi(self):
         if self.bmi()<18.5:
             return "niedowaga"
@@ -32,3 +32,11 @@ class Osoba:
             return "nadwaga"
         else:
             return "otyłość"
+        
+    def policz_ppm(self,plec):
+        if plec == "k":
+            return 655.1 + 9.563*self.waga + 1.85*self.wzrost -4.676*self.wiek
+        elif plec == "m":
+            return 66.5 + 13.75 * self.waga + 5.003 * self.wzrost - 6.775 * self.wiek
+        else:
+            return "wpisz k - kobieta, m - mężczyzna"
