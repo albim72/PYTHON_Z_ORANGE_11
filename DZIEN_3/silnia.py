83f0d3c9610e8c4deb2ae467793879539e09986a
albim72 <marcin.albiniak72@gmail.com>
2022-11-16 08:42:10 +0100

    Create silnia.py

---
+++ DZIEN_3/silnia.py
@@ -0,0 +1,20 @@
+import sys
+
+#n!=1*2*3*...*n
+#double -> 1.8E+308 , min 2.4E-324
+#???n -> 171!
+sys.set_int_max_str_digits(1000000)
+def silnia(n):
+    if n<0:
+        raise ValueError("silnia nie jest zdefiniowana dla liczb ujemnych")
+    wynik = 1
+    for i in range(1,n+1):
+        wynik*=i
+    return wynik
+
+
+try:
+    n=int(input("podaj wartość argumentu n: "))
+    print(f'silnia z n={n} wynosi: {silnia(n)}')
+except ValueError as ve:
+    print(ve)
