37a0b3edea581c2fd67ae9250409809be4493c4c
albim72 <marcin.albiniak72@gmail.com>
2022-11-16 08:16:33 +0100

    Create fileerror.py

---
+++ DZIEN_3/fileerror.py
@@ -0,0 +1,21 @@
+import sys
+
+try:
+    f = open("waznedane.txt","r",encoding="utf-8")
+    s = f.readline()
+    i = int(s.strip())
+    raise Exception(d=i/0)
+except OSError as err:
+    print(f"błąd systemowy: {err}")
+except ValueError:
+    print("nie można przekonwertować danych na typ int")
+except Exception as exc:
+    print(f"klasa błędu: {type(exc)}")
+    print(exc.args)
+    print(exc)
+except:
+    print(f"nieoczekiwany błąd -> {sys.exc_info()[0]}")
+else:
+    print("plik odczytany")
+finally:
+    print("koniec analizy błędu...")
